f02db69ec8e994c6686b043c4b768c1381a5aa5b
chengchen <chch4021642@sina.com>
2020-08-28 17:50:41 +0800

    Initial commit

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-service/src/main/java/DemoProjectApplication.java
@@ -0,0 +1,40 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import ${package}.util.ErrorUtil;
+import ${groupId}.middleware.common.Result;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@ControllerAdvice
+@EnableSwagger2
+@EnableFeignClients
+@EnableDiscoveryClient
+public class DemoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoProjectApplication.class, args);
+    }
+
+    /**
+     * 全局异常处理
+     *
+     * @param ex
+     *            Exception
+     * @return R
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public Result<String> defaultExceptionHandler(Exception ex) {
+        return ErrorUtil.handException(ex);
+    }
+}
